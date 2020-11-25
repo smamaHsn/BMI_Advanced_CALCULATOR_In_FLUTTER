664b2c411639c0582de6b066fa91b370be8c03eb
smama <smamahsn@gmail.com>
2020-11-25 20:38:21 +0500

    Main page + Next page buttons color changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeight = 80.0;
-const kActiveCardColour = Color(0xFF1D1E33);
-const kInactiveCardColour = Color(0xFF111328);
+const kActiveCardColour = Color(0xFF1565C0);
+const kInactiveCardColour = Color(0xFF00B0FF);
 const kBottomContainerColour = Color(0xFF01579B);
 
 const kLabelTextStyle = TextStyle(
