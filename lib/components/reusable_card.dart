a1879c8b9789df57e07ad606d7168bcbc17b42db
smama <smamahsn@gmail.com>
2020-11-25 20:59:03 +0500

    Home page Buttons corner curved

--- lib/components/reusable_card.dart
+++ lib/components/reusable_card.dart
@@ -13,10 +13,10 @@ class ReusableCard extends StatelessWidget {
       onTap: onPress,
       child: Container(
         child: cardChild,
-        margin: EdgeInsets.all(15.0),
+        margin: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
           color: colour,
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(40.0),
         ),
       ),
     );
