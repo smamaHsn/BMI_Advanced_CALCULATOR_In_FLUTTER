41e0194c63e96fe4f39d9b93c3e5dd7aaa2c2c54
smama <smamahsn@gmail.com>
2020-11-25 20:45:00 +0500

    Gender Name Replaced

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -48,7 +48,7 @@ class _InputPageState extends State<InputPage> {
                       : kInactiveCardColour,
                   cardChild: IconContent(
                     icon: FontAwesomeIcons.mars,
-                    label: 'MALE',
+                    label: 'MEN',
                   ),
                 ),
               ),
@@ -64,7 +64,7 @@ class _InputPageState extends State<InputPage> {
                       : kInactiveCardColour,
                   cardChild: IconContent(
                     icon: FontAwesomeIcons.venus,
-                    label: 'FEMALE',
+                    label: 'WOMEN',
                   ),
                 ),
               ),
