04d360ebe4c0ddb6ae88782a16a874238c283c63
smama <smamahsn@gmail.com>
2020-11-25 20:29:16 +0500

    Main Screen Background color changed

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xFF00695C),
+        scaffoldBackgroundColor: Color(0xFF004D40),
       ),
       home: InputPage(),
     );
